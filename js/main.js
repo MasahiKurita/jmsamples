c5a04dc41b67401314fee421d64c73fb05363f38
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-04 18:28:05 +0900

    debug sample3

--- js/main.js
+++ js/main.js
@@ -136,50 +136,46 @@ $(document).bind("pageinit", function() {
 
         window.fbAsyncInit = function() {
             try {
-            FB.init({
-              appId      : "698356506895047", // App ID
-              status     : true, // check login status
-              cookie     : true, // enable cookies to allow the server to access the session
-              xfbml      : true  // parse XFBML
-            });
-
-            FB.Event.subscribe('auth.authResponseChange', function(response) {
-              if (response.status === 'connected') {
-                console.log("already");
-                console.log("userID: " + response.authResponse.userID);
-                FB.api('/' + response.authResponse.userID + '/permissions', 'get', {"access_token": response.authResponse.accessToken}, function(response2) {
-                    console.log(response2);
+                FB.init({
+                    appId      : "698356506895047", // App ID
+                    status     : true, // check login status
+                    cookie     : true, // enable cookies to allow the server to access the session
+                    xfbml      : true  // parse XFBML
                 });
-                testAPI();
-                console.log("already");
-              } else if (response.status === 'not_authorized') {
-                console.log("login1");
-                FB.login();
-                console.log("login1");
-              } else {
-                console.log("login1");
-                FB.login();
-                console.log("login1");
-              }
-            });
+
+//              FB.Event.subscribe('auth.authResponseChange', function(response) {
+                FB.getLoginStatus(function(response){
+                    if (response.status === 'connected') {
+                        console.log("userID: " + response.authResponse.userID);
+                        FB.api('/' + response.authResponse.userID + '/permissions', 'get', {"access_token": response.authResponse.accessToken}, function(response2) {
+                            console.log(response2);
+                        });
+                        testAPI();
+                    } else if (response.status === 'not_authorized') {
+                        FB.login();
+                    } else {
+                        FB.login();
+                    }
+                });
             } catch(e) {
                 console.log(e);
             }
 
-          };
-          (function(d){
-             try {
-             var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-             if (d.getElementById(id)) {return;}
-             js = d.createElement('script'); js.id = id; js.async = true;
-             js.src = "//connect.facebook.net/en_US/all.js";
-             ref.parentNode.insertBefore(js, ref);
-             } catch (e) {
-                 console().log(e);
-             }
-          }(document));
+        };
 
-          function testAPI() {
+        (function(d){
+            try {
+                var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+                if (d.getElementById(id)) {return;}
+                    js = d.createElement('script'); js.id = id; js.async = true;
+                    js.src = "//connect.facebook.net/en_US/all.js";
+                    ref.parentNode.insertBefore(js, ref);
+            } catch (e) {
+                console().log(e);
+            }
+        }(document));
+
+        function testAPI() {
               console.log('Welcome!  Fetching your information.... ');
               try {
               FB.api('/me/checkins', function(response) {
@@ -191,11 +187,11 @@ $(document).bind("pageinit", function() {
               } catch (e) {
                   console.log(e);
               }
-          };
+        };
 
-          $("button#fblogout").on({
+        $("button#fblogout").on({
               click: FB.logout()
-          });
+        });
 
     });
 
