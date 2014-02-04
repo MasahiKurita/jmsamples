e8d9539f06bcab4328e57ad8d5b8ef435a4f00a3
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-04 15:07:16 +0900

    debug sample3

--- js/main.js
+++ js/main.js
@@ -172,6 +172,7 @@ $(document).bind("pageinit", function() {
               console.log('Welcome!  Fetching your information.... ');
               try {
               FB.api('/me/checkins', function(response) {
+                  console.log("success to get checkins");
                 for(i=0; i<response.data.length; i++){
                     console.log('you checked in, ' + response.data[i].place.name + '.');
                 }
