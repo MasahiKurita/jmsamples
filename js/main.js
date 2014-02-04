040d5f3a20e50c4c2220670258842f08c8c55d80
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-04 14:57:38 +0900

    debug sample3

--- js/main.js
+++ js/main.js
@@ -146,9 +146,9 @@ $(document).bind("pageinit", function() {
               if (response.status === 'connected') {
                 testAPI();
               } else if (response.status === 'not_authorized') {
-                FB.login(, {scope: 'user_checkins'});
+                FB.login(function(response){}, {scope: 'user_status'});
               } else {
-                FB.login(, {scope: 'user_checkins'});
+                FB.login(function(response){}, {scope: 'user_status'});
               }
             });
             } catch(e) {
