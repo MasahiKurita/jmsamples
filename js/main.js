694d6abd4902e49e7372d3f4ab895b4ef9f504bd
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-04 14:11:53 +0900

    initilize facebook login sample

--- js/main.js
+++ js/main.js
@@ -64,10 +64,8 @@ $(document).bind("pageinit", function() {
         try {
 
             // XXX DEBUG
-            var myLatLng = new google.maps.LatLng(0, -180);
             var mapOptions = {
                 zoom: 3,
-                center: myLatLng,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
@@ -133,4 +131,50 @@ $(document).bind("pageinit", function() {
 //        map.addOverlay(new google.maps.Marker(latlng_3));
 
     });
+
+    $("div#sample3").bind("pageshow", function() {
+
+        window.fbAsyncInit = function() {
+            try {
+            FB.init({
+              appId      : "698356506895047", // App ID
+              status     : true, // check login status
+              cookie     : true, // enable cookies to allow the server to access the session
+              xfbml      : true  // parse XFBML
+            });
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+              if (response.status === 'connected') {
+                testAPI();
+              } else if (response.status === 'not_authorized') {
+                FB.login();
+              } else {
+                FB.login();
+              }
+            });
+            } catch(e) {
+                console.log(e);
+            }
+
+          };
+          (function(d){
+             try {
+             var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement('script'); js.id = id; js.async = true;
+             js.src = "//connect.facebook.net/en_US/all.js";
+             ref.parentNode.insertBefore(js, ref);
+             } catch (e) {
+                 console().log(e);
+             }
+          }(document));
+
+          function testAPI() {
+              console.log('Welcome!  Fetching your information.... ');
+              FB.api('/me', function(response) {
+                console.log('Good to see you, ' + response.name + '.');
+              });
+          };
+
+    });
+
 });
