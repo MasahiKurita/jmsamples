c5de1d205cde46e4df43309b3f4965b66fe44c85
x-masashik <x-masashik@JPC20152408.jp.sony.com>
2014-02-10 16:47:28 +0900

    debug sample3

--- js/main.js
+++ js/main.js
@@ -222,7 +222,7 @@ $(document).bind("pageinit", function() {
                       for(i=0; i<response.data.length; i++){
                           var data = response.data[i];
                           var place = data.place;
-                          var created_time = new Date((data.created_time);
+                          var created_time = new Date(data.created_time);
                           var datestr = created_time.getFullYear + "/" + (created_time.getMonth()+1) + "/" created_time.getDate();
                           checkinlist.append("<li>" + datestr + " に、" + place.name + "にチェックインしました。")</li>");
 
